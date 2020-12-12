36df50022ead825a6a43ece509393d779a65d1cd
root <root@ubuntu2004.linuxvmimages.local>
2020-12-11 22:20:08 -0500

    First commint with edited index.php and cover.css files

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Test CTF. Design based on bootstrap 'Cover' example">
+        <meta name="author" content="Drash">
+        <meta  >
+        <title>NPCTF - Darsh</title>
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="cover.css" rel="stylesheet">
+    </head>
+    <body class="text-center">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <header class="masthead mb-auto">
+            <div class="inner">
+            <h3 class="masthead-brand">NPCTF</h3>
+            <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Login</a>
+            </nav>
+            </div>
+            </header>
+
+            <main role="main" class="inner cover">
+            <h1 class="cover-heading">Wellcome Guys!</h1>
+            <p class="lead">Hope you are enjoying all the challenges so far. Lets, see what we got here on this page. Hint - Colours are beautifull!</p>
+            <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary">View Page Sourse</a>
+            </p>
+            </main>
+
+            <footer class="mastfoot mt-auto">
+                <div class="inner">
+                    <p>Made with ♥️ by #Itachi09.</p>
+                </div>
+            </footer>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    </body>
+</html>
